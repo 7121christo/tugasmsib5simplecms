56772b5c1e8f512941330213f032eda542611e5c
CHRadyatma <93330711+7121christo@users.noreply.github.com>
2023-10-21 02:01:06 +0700

    minorchanges

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,31 +1,21 @@
-@extends('layouts.app')
+@extends('layouts.base')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-
-    <table border=1 >
-        <tr align="center">
-            <th>ID</th>
-            <th>Nama</th>
-            <th>Deskripsi</th>
-            <th colspan="2">Actions</th>
-        </tr>
-        @foreach ( $kategoris as $kategori )
+        <table border="1">
             <tr align="center">
-                <td >{{ $kategori->id }}</td>
-                <td ><a href="{{ route("show", $kategori->id) }}">{{ $kategori->namaKategori }}</a></td>
-                <td >{{ $kategori->deskripsi }}</td>
-                <td >
+                <th>ID</th>
+                <th>Nama</th>
+                <th>Deskripsi</th>
+                <th colspan="2">Actions</th>
+            </tr>
+            @foreach($kategoris as $kategori)
+            <tr align="center">
+                <td>{{ $kategori->id }}</td>
+                <td><a href="{{ route("show", $kategori->id) }}">{{ $kategori->namaKategori }}</a></td>
+                <td>{{ $kategori->deskripsi }}</td>
+                <td>
                     <form action="{{ route('edit', $kategori) }}" method="get">
                         @csrf
                         <button type="submit">Edit</button>
@@ -39,16 +29,13 @@
                     </form>
                 </td>
             </tr>
-        @endforeach
-        <tr>
-            <td colspan="5" align="right">
-                <a href="{{ route('create') }}"><button type="submit">Create</button></a>
-            </td>
-        </tr>
-    </table>
-
-</body>
-</html>
+            @endforeach
+            <tr>
+                <td colspan="5" align="right">
+                    <a href="{{ route('create') }}"><button type="submit">Create</button></a>
+                </td>
+            </tr>
+        </table>
     </div>
 </div>
 @endsection
